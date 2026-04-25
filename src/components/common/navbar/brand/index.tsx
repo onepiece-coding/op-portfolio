4204d1a9b136051a12df593b2967da729dafbb79
onepiece-coding <makclaaax@gmail.com>
2026-04-25 20:54:20 +0100

    theme changed

--- src/components/common/navbar/brand/index.tsx
+++ src/components/common/navbar/brand/index.tsx
@@ -2,28 +2,28 @@
  * @file src/components/common/navbar/brand/index.tsx
  */
 
-import { useTranslation } from "react-i18next";
+// import { useTranslation } from "react-i18next";
 
 import styles from "./styles.module.css";
 
 const NavBrand = () => {
-  const { t } = useTranslation();
+  // const { t } = useTranslation();
 
   return (
     <div className={styles.brand}>
-      <div className={styles.logo} aria-hidden="true">
+      {/* <div className={styles.logo} aria-hidden="true">
         OP
-      </div>
-      {/* <img
+      </div> */}
+      <img
         alt="OnePiece Coding"
-        src="/op-portfolio/logo-test.png"
+        src="/op-portfolio/dark-logo.png"
         style={{
-          width: "100px",
-          marginTop: "4px",
-          marginLeft: "-16px",
+          width: "200px",
+          marginTop: "7px",
+          marginLeft: "-24px",
         }}
-      /> */}
-      <div
+      />
+      {/* <div
         style={{
           // marginLeft: "-16px",
           marginTop: "-4px",
@@ -31,7 +31,7 @@ const NavBrand = () => {
       >
         <div className={styles.brandName}>{t("brand.name")}</div>
         <div className={styles.brandTagline}>{t("brand.tagline")}</div>
-      </div>
+      </div> */}
     </div>
   );
 };
